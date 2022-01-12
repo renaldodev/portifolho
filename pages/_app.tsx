ff1a868be2d3a0d147ca63c7da86f652f026b441
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-01-12 01:35:44 +0100

    theme chakra provider add

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import type { AppProps } from 'next/app'
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
 
-export default MyApp
+    return (
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    )
+  }
+  
+  export default MyApp
