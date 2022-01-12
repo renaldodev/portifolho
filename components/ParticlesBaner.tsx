c671780071a355bc516431621417fa496f1f0b85
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-01-12 02:05:16 +0100

    Particles error correct add

--- components/ParticlesBaner.tsx
+++ components/ParticlesBaner.tsx
@@ -2,20 +2,9 @@ import Particles from "react-tsparticles";
 
 export default function
  ParticlesBaner(){
-  const particlesInit = (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
  return (
         <Particles
       id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
       options={{
         background: {
           color: {
