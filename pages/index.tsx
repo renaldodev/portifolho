46ebf81e3743a1f76c4a7ccd2a2d1ecb3339e39a
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-01-14 20:28:43 +0100

    Desktop nav

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,12 @@
 import type {NextPage } from "next";
-import { Text, Heading } from "@chakra-ui/react";
-import Link from "next/link";
 import ParticlesBaner from "../components/ParticlesBaner";
-import Banner, { BannerImage } from "../components/Banner";
+import Banner from "../components/Banner";
 import Hero from "@components/Hero";
 import About from "@components/About";
 import Project from "@components/Project";
 import Contact from "@components/Contact";
 import useScrollDirection from "@hooks/useScrollDirection";
 const Home: NextPage = () => {
-  const direction=useScrollDirection()
-  console.log(direction)
   return (
     <>
       <Banner>
