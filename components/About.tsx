aa94b9f4017f40907ca07e0ed648f5e292c114d5
Renaldo Mateus <renaldomateus77.dev@gmail.com>
2022-01-17 10:13:29 +0100

    Vim alter

--- components/About.tsx
+++ components/About.tsx
@@ -3,9 +3,11 @@ import styled from "@emotion/styled";
 const AboutStyles = styled(Flex)`
   min-height: 100vh;
 `;
+
 export default function About() {
   return <AboutStyles>
       <Heading size={'5xl'} id="About">
+
           About1
       </Heading>
   </AboutStyles>;
